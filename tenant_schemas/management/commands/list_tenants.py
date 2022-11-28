d01e7d89eb0e8ac48b2c1aa86bb34b0697ae9201
pcamposti <88953580+pcamposti@users.noreply.github.com>
2022-11-28 16:17:29 -0300

    Update list_tenants.py
    add db command

--- tenant_schemas/management/commands/list_tenants.py
+++ tenant_schemas/management/commands/list_tenants.py
@@ -7,11 +7,13 @@ from tenant_schemas.utils import get_tenant_model
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
+        database = options.get('database', 'default')
         columns = ('schema_name', 'domain_url')
 
         TenantModel = get_tenant_model()
-        all_tenants = TenantModel.objects.values_list(*columns)
+        all_tenants = TenantModel.objects.using(db).values_list(*columns)
 
         out = csv.writer(sys.stdout, dialect=csv.excel_tab)
         for tenant in all_tenants:
             out.writerow(tenant)
+        super(Command, self).handle(*args, **options)
