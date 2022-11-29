44d37da708986f29544ef655c21594f15d6cd699
pcamposti <88953580+pcamposti@users.noreply.github.com>
2022-11-29 13:02:36 -0300

    parallel echo wait

--- tenant_schemas/migration_executors/parallel.py
+++ tenant_schemas/migration_executors/parallel.py
@@ -12,16 +12,18 @@ class ParallelExecutor(MigrationExecutor):
 
     def run_tenant_migrations(self, tenants):
         db = self.options.get('db', None) or self.options.get('database', None)
-        os.system('pwd')
-        os.system('ls')
         os.system(f"echo {db}")
         if tenants:
             count = 0
             for tenant in tenants:
+                os.system(f"echo {tenant}")
                 count += 1
+                os.system(f"echo python manage.py migrate_schemas tenant --database={db} --schema={tenant}")
                 os.system(f'python manage.py migrate_schemas tenant --database={db} --schema={tenant} &')
+                os.system(f"echo python manage.py migrate_schemas commons_pg --database={db} --schema={tenant}")
                 os.system(f'python manage.py migrate_schemas commons_pg --database={db} --schema={tenant} &')
                 if count == 5:
                     count = 0
+                    os.system(f"echo wait {str(count)}")
                     os.system('wait')
             os.system('wait')
