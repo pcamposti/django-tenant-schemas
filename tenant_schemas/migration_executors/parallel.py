cbaf4951c7d4e78d2fa67102086a9bb0a3435cd9
pcamposti <88953580+pcamposti@users.noreply.github.com>
2022-11-29 16:51:05 -0300

    fix wait

--- tenant_schemas/migration_executors/parallel.py
+++ tenant_schemas/migration_executors/parallel.py
@@ -14,6 +14,7 @@ class ParallelExecutor(MigrationExecutor):
     def run_tenant_migrations(self, tenants):
         db = self.options.get('db', None) or self.options.get('database', None)
         os.system(f"echo {db}")
+        os.system(f"echo {self.args}")
         if tenants:
             count = 0
             command = ''
@@ -23,7 +24,7 @@ class ParallelExecutor(MigrationExecutor):
                 #command += f'python manage.py migrate_schemas commons_pg --database={db} --schema={tenant} & \n'
                 count += 1
                 migrate_tenant = subprocess.Popen(['python', 'manage.py', 'migrate_schemas', 'tenant', f'--database={db}', f'--schema={tenant}'])
-                migrate_tenant = subprocess.Popen(['python', 'manage.py', 'migrate_schemas', 'commons_pg', f'--database={db}', f'--schema={tenant}'])
+                migrate_commons = subprocess.Popen(['python', 'manage.py', 'migrate_schemas', 'commons_pg', f'--database={db}', f'--schema={tenant}'])
                 os.system(f"echo python manage.py migrate_schemas tenant --database={db} --schema={tenant} ")
                 os.system(f"echo python manage.py migrate_schemas commons_pg --database={db} --schema={tenant}")
                 if count == 5:
